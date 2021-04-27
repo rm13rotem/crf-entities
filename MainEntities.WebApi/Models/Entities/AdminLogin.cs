0f60f0665c78556eb546e99e39121da0f484d442
Rotem.M <Rotem.M@DEV-2>
2021-04-27 18:29:39 +0300

    First template rebase

---
+++ MainEntities.WebApi/Models/Entities/AdminLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainEntities.WebApi.Models.Entities
+{
+    public class AdminLogin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string YearOfBirth { get; set; }
+        public string DoctorNumber { get; set; }
+    }
+}
